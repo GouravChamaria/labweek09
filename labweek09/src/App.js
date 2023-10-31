ef386e7dd5fa74e87177b9580e2bd2b560d5777c
GouravChamaria <gouravch794@gmail.com>
2023-10-31 11:43:38 +0530

    Lab done!

---
+++ labweek09/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to full Stack Development -1</p>
+        <p>ReactJs programming Week09 Lab Exercise</p>
+        <p>StudentId 101354494</p>
+        <p>Gourav Chamaria</p>
+        <p>GeorgeBrownCollege, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
